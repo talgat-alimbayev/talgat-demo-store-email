4241810d561ccc0a4eb1b2f52bdddc3302221741
talimbayev <alimbayev.talgat@gmail.com>
2022-12-30 18:16:32 +0600

    added blank project

---
+++ src/main/java/talgatdemostoreemail/TalgatDemoStoreEmailApplication.java
@@ -0,0 +1,13 @@
+package talgatdemostoreemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalgatDemoStoreEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalgatDemoStoreEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talgatdemostoreemail/TalgatDemoStoreEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package talgatdemostoreemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalgatDemoStoreEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
