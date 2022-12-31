9b0b6e5604100ff57e3a666d3f8bdea118240212
talimbayev <alimbayev.talgat@gmail.com>
2022-12-31 12:47:57 +0600

    just a first draft, sends emails

--- src/main/java/talgatdemostoreemail/TalgatDemoStoreEmailApplication.java
+++ src/main/java/talgatdemostoreemail/TalgatDemoStoreEmailApplication.java
@@ -1,7 +1,11 @@
 package talgatdemostoreemail;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import talgatdemostoreemail.services.EmailSenderService;
 
 @SpringBootApplication
 public class TalgatDemoStoreEmailApplication {
@@ -10,4 +14,13 @@ public class TalgatDemoStoreEmailApplication {
 		SpringApplication.run(TalgatDemoStoreEmailApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner emailSender(EmailSenderService senderService){
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				senderService.sendEmail("alimbayev.talgat@gmail.com","hello","1st email sent by Spring Boot");
+			}
+		};
+	}
 }
